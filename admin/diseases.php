3879cfed3a31f30967249802f517dc9208f68c99
yernar707 <ernarergaziev00@gmail.com>
2021-12-08 19:11:58 +0600

    style bug fixed

--- admin/diseases.php
+++ admin/diseases.php
@@ -30,7 +30,7 @@
 <head>
 	<title>Beast Mode</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="//style/style.css">
+	<link rel="stylesheet" type="text/css" href="/style/style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
